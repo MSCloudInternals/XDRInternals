08cb97d1b909889088149618c59e2250f115051b
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-12-03 21:50:16 -0900

    Remove-Variable is bad

--- XDRInternals/functions/Set-XdrConnectionSettings.ps1
+++ XDRInternals/functions/Set-XdrConnectionSettings.ps1
@@ -46,17 +46,15 @@
         Write-Verbose "SccAuth is secure string, converting to plain text"
         $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SccAuth)
         try {
-            Remove-Variable SccAuth
             $SccAuth = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
         } finally {
             [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
         }
     }
     if ($Xsrf -is [System.Security.SecureString]) {
-         Write-Verbose "Xsrf is secure string, converting to plain text"
+        Write-Verbose "Xsrf is secure string, converting to plain text"
         $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Xsrf)
         try {
-            Remove-Variable Xsrf
             $Xsrf = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
         } finally {
             [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)

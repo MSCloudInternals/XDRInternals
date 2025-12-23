28db7bcf40e407b1690d1fd1d89552c12e9418a1
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-12-22 22:50:29 -0900

    Add Get-XdrAdvancedHuntingUnifiedDetectionRules and support for changing TenantId

--- XDRInternals/functions/Set-XdrConnectionSettings.ps1
+++ XDRInternals/functions/Set-XdrConnectionSettings.ps1
@@ -37,43 +37,50 @@
         [Parameter(Mandatory, ParameterSetName = 'Manual')]
         $Xsrf,
 
+        [Parameter(ParameterSetName = 'Manual')]
+        [Parameter(ParameterSetName = 'TenantId')]
+        $TenantId,
+
         [Parameter(Mandatory, ParameterSetName = 'Websession')]
         [Microsoft.PowerShell.Commands.WebRequestSession]$WebSession
     )
 
-    # Convert secure strings to plain text
-    if ($SccAuth -is [System.Security.SecureString]) {
-        Write-Verbose "SccAuth is secure string, converting to plain text"
-        $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SccAuth)
-        try {
-            $SccAuthValue = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-        } finally {
-            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
-        }
-    } else {
-        $SccAuthValue = $SccAuth
-    }
-    if ($Xsrf -is [System.Security.SecureString]) {
-        Write-Verbose "Xsrf is secure string, converting to plain text"
-        $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Xsrf)
-        try {
-            $XsrfValue = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-        } finally {
-            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
-        }
-    } else {
-        $XsrfValue = $Xsrf
-    }
-    
+    # Determine sccauth and xsrf format, then create session and cookies
     Write-Verbose "Setting session cookies for XDR webpage requests"
-    if ($PSBoundParameters.ContainsKey('WebSession')) {
-        # Use the provided WebSession instead of creating a new one
-        $script:session = $WebSession
-    } else {
+    if ($PSBoundParameters.ContainsKey('SccAuth')) {
+        if ($SccAuth -is [System.Security.SecureString]) {
+            Write-Verbose "SccAuth is secure string, converting to plain text"
+            $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SccAuth)
+            try {
+                $SccAuthValue = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+            } finally {
+                [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+            }
+        } else {
+            $SccAuthValue = $SccAuth
+        }
+        if ($Xsrf -is [System.Security.SecureString]) {
+            Write-Verbose "Xsrf is secure string, converting to plain text"
+            $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Xsrf)
+            try {
+                $XsrfValue = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+            } finally {
+                [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+            }
+        } else {
+            $XsrfValue = $Xsrf
+        }
+
         # Create session and cookies
         $script:session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
         $script:session.Cookies.Add((New-Object System.Net.Cookie("sccauth", $SccAuthValue, "/", "security.microsoft.com")))
         $script:session.Cookies.Add((New-Object System.Net.Cookie("XSRF-TOKEN", $XsrfValue, "/", "security.microsoft.com")))
+    }
+    
+    
+    if ($PSBoundParameters.ContainsKey('WebSession')) {
+        # Use the provided WebSession instead of creating a new one
+        $script:session = $WebSession
     }
 
     # Set the headers to include the xsrf token
@@ -84,6 +91,16 @@
     # Cache the XSRF token with 5 minute TTL
     Write-Verbose "Caching XSRF token with 5 minute TTL"
     Set-XdrCache -CacheKey "XsrfToken" -Value $script:headers["X-XSRF-TOKEN"] -TTLMinutes 5
+
+    # Set TenantId in cache and headers
+    Write-Verbose "Caching TenantId with 1 day TTL"
+    if ($PSBoundParameters.ContainsKey('TenantId')) {
+        Set-XdrCache -CacheKey "XdrTenantId" -Value $TenantId -TTLMinutes 1440
+    } else {
+        $TenantId = (Get-XdrTenantContext).AuthInfo.TenantId
+        Set-XdrCache -CacheKey "XdrTenantId" -Value $TenantId -TTLMinutes 1440
+    }
+    $script:headers["X-Tid"] = $TenantId
     
     Write-Host "XDR Connection Settings created"
     Write-Host "You can now run other XDRInternals cmdlets to interact with the XDR portal."
